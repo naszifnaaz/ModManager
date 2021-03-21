4f0a041f138595aa5aaff816cc3b6d18e203cee2
Muhammed Nasif <73707865+naszifnaaz@users.noreply.github.com>
2021-03-21 20:20:33 +0530

    OpenMWExport
    Mod manager for openmw

---
+++ OpenMWExport.py
@@ -0,0 +1,161 @@
+# This Mod Organizer plugin is released to the pubic under the terms of the GNU GPL version 3, which is accessible from the Free Software Foundation here: https://www.gnu.org/licenses/gpl-3.0-standalone.html
+
+# To use this plugin, place it in the plugins directory of your Mod Organizer install (ideally along with the OpenMW icon). You will then find an 'Export to OpenMW' option under the tools menu.
+
+# If someone wanted to, they could expand upon this by registering callbacks for mod and plugin state changes so that you didn't manually have to click the button any more to make this work.
+
+# Also, if Mod Organizer ever re-implements archive handling, then it wouldn't be too hard to make this also copy the list of these to fallback-archive= lines.
+
+from pathlib import Path
+import sys
+
+from PyQt5.QtCore import QCoreApplication, QStandardPaths, QUrl, qCritical
+from PyQt5.QtGui import QDesktopServices, QIcon
+from PyQt5.QtWidgets import QFileDialog, QMessageBox
+
+if "mobase" not in sys.modules:
+    import mobase
+
+class OpenMWExportPlugin(mobase.IPluginTool):
+    
+    def __init__(self):
+        super(OpenMWExportPlugin, self).__init__()
+        self.__organizer : mobase.IOrganizer
+        self.__nexusBridge : mobase.IModRepositoryBridge
+
+    def init(self, organizer):
+        self.__organizer = organizer
+        if sys.version_info < (3, 6):
+            qCritical(self.__tr("OpenMWExport plugin requires a Python {0} interpreter or newer, but is running on a Python {1} interpreter.").format("3.6", ".".join(map(str, sys.version_info[:3]))))
+            QMessageBox.critical(self._parentWidget(), self.__tr("Incompatible Python version."), self.__tr("This version of the OpenMW Export plugin requires a Python {0} interpreter or newer, but Mod Organizer has provided a Python {1} interpreter. Mod Organizer {2} is the earliest compatible version. You should check for an update.").format("3.6", ".".join(map(str, sys.version_info[:3])), "2.1.6"))
+            return False
+        if self.__organizer.appVersion() >= mobase.VersionInfo(2, 3, 1):
+            self.__nexusBridge = self.__organizer.createNexusBridge()
+            self.__nexusBridge.descriptionAvailable.connect(self.__onDescriptionReceived)
+            self.__organizer.onUserInterfaceInitialized(self.__checkForUpdate)
+        return True
+
+    def name(self):
+        return "OpenMW Exporter"
+
+    def author(self):
+        return "AnyOldName3"
+
+    def description(self):
+        return self.__tr("Transfers mod list (left pane) to data fields in OpenMW.cfg and plugin list (right pane, plugins tab) to content fields in OpenMW.cfg. This allows you to run OpenMW with the current profile's setup from outside of Mod Organizer")
+
+    def version(self):
+        return mobase.VersionInfo(3, 0, 0, mobase.ReleaseType.FINAL)
+
+    def isActive(self):
+        return (self.__organizer.managedGame().gameName() == "Morrowind")
+
+    def settings(self):
+        return []
+
+    def displayName(self):
+        return self.__tr("Export to OpenMW")
+
+    def tooltip(self):
+        return self.__tr("Exports the current mod list and plugin load order to OpenMW.cfg")
+
+    def icon(self):
+        return QIcon("plugins/openmw.ico")
+
+    def display(self):
+        # We should test if the current game is compatible with OpenMW here
+        # We can't do that directly, so instead we just test if the current game is Morrowind
+        game = self.__organizer.managedGame()
+        if game.gameName() != "Morrowind":
+            QMessageBox.critical(self._parentWidget(), self.__tr("Incompatible game"), self.__tr("(At least when this plugin is being written) OpenMW only supports game data designed for the Morrowind engine. The game being managed is not Morrowind, so the export will abort. If you think you know better than this message, update this plugin."))
+            return
+        # Give the user the opportunity to abort
+        confirmationButton = QMessageBox.question(self._parentWidget(), self.__tr("Before starting export..."), self.__tr("Before starting the export to OpenMW, please ensure you've backed up anything in OpenMW.cfg which you do not want to risk losing forever."), QMessageBox.StandardButtons(QMessageBox.Ok | QMessageBox.Cancel))
+        if confirmationButton != QMessageBox.Ok:
+            return
+        # Get the path to the OpenMW.cfg file
+        configPath = self.__getOpenMWConfigPath()
+        if not configPath.is_file():
+            QMessageBox.critical(self._parentWidget(), self.__tr("Config file not specified"), self.__tr("No config file was specified"))
+            return
+        # Clear out the existing data= and content= lines from openmw.cfg
+        self.__clearOpenMWConfig(configPath)
+        with configPath.open("a", encoding="utf-8") as openmwcfg:
+            # write out data directories
+            openmwcfg.write(self.__processDataPath(game.dataDirectory().absolutePath()))
+            for mod in self.__organizer.modsSortedByProfilePriority():
+                self.__processMod(openmwcfg, mod)
+            self.__processMod(openmwcfg, "Overwrite")
+            
+            # write out content (plugin) files
+            # order content files by load order
+            loadOrder = {}
+            for plugin in self.__organizer.pluginList().pluginNames():
+                loadIndex = self.__organizer.pluginList().loadOrder(plugin)
+                if loadIndex >= 0:
+                    loadOrder[loadIndex] = plugin
+            # actually write out the list
+            for pluginIndex in range(len(loadOrder)):
+                openmwcfg.write("content=" + loadOrder[pluginIndex] + "\n")
+        QMessageBox.information(self._parentWidget(), self.__tr("OpenMW Export Complete"), self.__tr("The export to OpenMW completed successfully. The current setup was saved to {0}").format(configPath))
+    
+    def __tr(self, str):
+        return QCoreApplication.translate("OpenMWExportPlugin", str)
+    
+    def __processMod(self, configFile, modName):
+        state = self.__organizer.modList().state(modName)
+        if (state & 0x2) != 0 or modName == "Overwrite":
+            path = self.__organizer.getMod(modName).absolutePath()
+            configLine = self.__processDataPath(path)
+            configFile.write(configLine)
+    
+    def __processDataPath(self, dataPath):
+        # boost::filesystem::path uses a weird format in order to round-trip being constructed from a stream correctly, even when quotation characters are in the path
+        processedPath = "data=\""
+        for character in dataPath:
+            if character == '&' or character == '"':
+                processedPath += "&"
+            processedPath += character
+        processedPath += "\"\n"
+        return processedPath
+    
+    def __clearOpenMWConfig(self, configPath):
+        import tempfile
+        import os
+        import shutil
+        # copy the lines we want to keep to a temp file
+        tempFilePath = None
+        with tempfile.NamedTemporaryFile(mode="w", delete = False, encoding="utf-8") as f:
+            tempFilePath = f.name
+            lastLine = ""
+            with configPath.open("r", encoding="utf-8-sig") as openmwcfg:
+                for line in openmwcfg:
+                    if not line.startswith("data=") and not line.startswith("content="):
+                        f.write(line)
+                        lastLine = line
+            # ensure the last line ended with a line break
+            if not lastLine.endswith("\n"):
+                f.write("\n")
+        # we can't move to Path.replace due to https://bugs.python.org/issue29805
+        os.remove(configPath)
+        shutil.move(tempFilePath, configPath)
+    
+    def __getOpenMWConfigPath(self):
+        defaultLocation = Path(QStandardPaths.locate(QStandardPaths.DocumentsLocation, str(Path("My Games", "OpenMW", "openmw.cfg"))))
+        if defaultLocation.is_file():
+            return defaultLocation
+        # If we've got this far, then the user is doing something very weird, so they can find it themselves.
+        return Path(QFileDialog.getOpenFileName(self._parentWidget(), self.__tr("Locate OpenMW Config File"), ".", "OpenMW Config File (openmw.cfg)")[0])
+    
+    def __checkForUpdate(self, mainWindow):
+        self.__nexusBridge.requestDescription("Morrowind", 45642, None)
+
+    def __onDescriptionReceived(self, gameName, modID, userData, resultData):
+        version = mobase.VersionInfo(resultData["version"])
+        if self.version() < version:
+            response = QMessageBox.question(self._parentWidget(), self.__tr("Plugin update available"), self.__tr("{0} can be updated from version {1} to {2}. Do you want to open the download page in your browser?").format(self.displayName(), self.version(), version))
+            if response == QMessageBox.Yes:
+                QDesktopServices.openUrl(QUrl("https://www.nexusmods.com/morrowind/mods/45642"))
+    
+def createPlugin():
+    return OpenMWExportPlugin()
--- README.md
+++ README.md
@@ -1,2 +1,86 @@
-# ModManager
-Mod Manager for Elder Scrolls Morrowind
+# ModOrganizer-to-OpenMW
+A Mod Organizer plugin to export your VFS, plugin selection and load order to OpenMW
+
+## Why you'd want this:
+
+* OpenMW has no GUI for mod directory installation and management.
+Mod Organizer does.
+* If you use OpenMW's VFS system to manage your mods, tools like MLOX can't see all your content files, so aren't much use.
+If you start these tools through Mod Organizer, it will inject its VFS into them and they'll be able to do their jobs properly.
+
+* Starting your game through Mod Organizer every time might be a bit of a hassle, but that's the only way to make a program see its VFS.
+OpenMW has its own VFS, so if you use it, you can just start OpenMW normally.
+* Mod Organizer (more specifically its VFS implementation, USVFS) has some issues that mean it doesn't always work as intended stemming from the complexity of injecting a VFS into a process only designed to load things from one directory.
+OpenMW's VFS is far more reliable because it's 'supposed to be there' and was therefore much simpler to write without bugs.
+
+## Installation:
+
+Put `OpenMWExport.py` and (optionally) `openmw.ico` in your Mod Organizer `plugins` directory.
+If you don't want to use this plugin in English and a translation `.qm` file exists for this plugin in your desired language, put it in your Mod Organizer `translations` directory.
+
+## Usage:
+
+You'll find an 'Export to OpenMW' option under the tools menu.
+Clicking it will make the plugin do its thing.
+If you need to do anything else, you'll get a message box explaining it.
+
+## Note:
+
+As of version 2.0.0, ModOrganizer-to-OpenMW has been upgraded to take advantage of Python 3.
+The upgrade to Python 3 is included in the MO 2.1.6 release, and older versions are not compatible with the latest version of this plugin.
+If MO 2.1.5 is still the latest release, you'll probably have to visit the Mod Organizer Discord server to get a release candidate build to use this.
+You can still use older versions of this plugin in older Mod Organizer builds (although MO 2.1.2 is the minimum which will work).
+
+## Translating this plugin:
+
+As of version 1.1, this plugin has a `.ts` file.
+This allows people who know multiple languages to create translations of it.
+You can contribute your own translations at https://www.transifex.com/anyoldname3/modorganizer-to-openmw and I'll try to include them in the next release.
+If you want to translate to a language that isn't listed there, there should be a Request Language button somewhere.
+
+## Building this plugin
+
+As this plugin is pure Python, it doesn't need building - you can just copy the files into place by hand.
+However, obviously that won't update the translation files, and can become annoying when working on the plugin, as you'll need to do it repeatedly.
+Because of that, some automation is provided:
+
+
+`install.ps1` will regenerate the `.ts` file and copy the plugin files to the location specified by its first argument or given via stdin.
+If the `-Release` flag is passed, it'll also copy the licence and readme, making it easier to do a release.
+To use it, you'll need a Python interpreter with some version of PyQt5's `PyQt5.pylupdate_main` package installed to be available in the current shell.
+If you've got a Mod Organizer 2 development environment on your machine, you can add its Python interpreter to the current shell's path, otherwise `pip install PyQt5` will get you what you need.
+
+Stubs for MO2's plugin API are available in the `mobase-stubs` pypi package
+
+By far the easiest way to get linting and pylupdate working is just to create a venv and install the packages in `requirements.txt`:
+```powershell
+python -m venv env
+.\env\scripts\activate
+python -m pip install -r requirements.txt
+```
+Once this venv exists, editors like VS Code should pick it up and use it automatically (although they might need restarting first).
+
+Also, a VS Code `tasks.json` file is provided that will call the install script as a build task.
+To specify the install destination, create a file in the `.vscode` directory called `InstallDestination.txt` containing the path to the MO2 plugins directory.
+It also includes another build task for calling the release script which uses `.vscode/ReleaseDestination.txt`.
+Linting won't work with its default `pylint` linter as native packages with stubs are used, and in mid-2020, `pylint` doesn't support stubs.
+However, `mypy` works just fine once it's aware of the stubs.
+Battles were waged to try and make `mypy` see the stubs when they weren't in a venv and it was too complicated to document and reliably reproduce.
+Just use the venv.
+
+You'll probably want to create a `.vscode/settings.json` resembling:
+```json
+{
+    "python.linting.pylintEnabled": false,
+    "python.linting.mypyEnabled": true,
+    "python.pythonPath": "env\\Scripts\\python.exe"
+}
+```
+This isn't checked in as you'll likely end up with local changes.
+It would be nice if JSON had include directives like YAML.
+
+## Future possibilities
+
+* As the Mod Organizer Morrowind plugin matures, this tool will gradually become more useful.
+* This plugin could be adapted to use callbacks so it runs automatically whenever you make any changes in Mod Organizer
+* If Mod Organizer ever re-implements archive management, this plugin could copy Mod Organizer's list to OpenMW's `fallback-archive` list.
